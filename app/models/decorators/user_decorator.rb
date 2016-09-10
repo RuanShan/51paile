aedf3cfdae7ec65f03b173309e2323619b3f3be0
david <areq22@aliyun.com>
2016-09-10 19:56:15 +0800

    add job auction

--- app/models/decorators/user_decorator.rb
+++ app/models/decorators/user_decorator.rb
@@ -1,13 +1,22 @@
 Spree::User.class_eval do
   has_many :auction_foregifts
+  has_many :bids, foreign_key: :bidder_id
   has_many :biddable_auctions, through: :auction_foregifts, source: :auction
 
   def biddable?( auction )
     biddable_auction_ids.include? auction.id
   end
 
+  def auction_bids( auction )
+    bids.where( auction: auction )
+  end
+
+  def auction_bid_count( auction )
+    auction_bids( auction ).count
+  end
+
   def auction_foregift( auction )
     auction_foregifts.where( auction: auction ).first
   end
-  
+
 end
